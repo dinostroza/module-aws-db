9ef4786a22b1238dc22c15c39836eb4fc2eebf42
mitraman <ronnie.mitra@gmail.com>
2020-07-26 19:02:05 +0100

    formatted

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ resource "aws_db_subnet_group" "rds-subnet-group" {
 
 # Create a security group to allow traffic from the EKS cluster
 resource "aws_security_group" "db-security-group" {
-  name = "${var.env_name}-allow-eks-db"
+  name        = "${var.env_name}-allow-eks-db"
   description = "Allow traffic from EKS managed workloads"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   ingress {
-    description = "All traffic from managed EKS"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description     = "All traffic from managed EKS"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = data.aws_eks_cluster.microservice-cluster.vpc_config.0.security_group_ids
   }
 }
@@ -36,15 +36,15 @@ resource "aws_db_instance" "mysql-db" {
   engine_version    = "5.7"
   instance_class    = "db.t2.micro"
   name              = var.mysql_database
-  identifier = "microservices-mysql"
-  
+  identifier        = "microservices-mysql"
+
   username             = var.mysql_user
   password             = var.mysql_password
   parameter_group_name = "default.mysql5.7"
 
-  skip_final_snapshot  = true
-  
-  db_subnet_group_name = aws_db_subnet_group.rds-subnet-group.name
+  skip_final_snapshot = true
+
+  db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
   vpc_security_group_ids = [aws_security_group.db-security-group.id]
 }
 
@@ -63,6 +63,6 @@ resource "aws_elasticache_cluster" "redis-db" {
   engine_version       = "3.2.10"
   port                 = 6379
 
-  subnet_group_name    = aws_elasticache_subnet_group.redis-subnet-group.name
-  security_group_ids   = [aws_security_group.db-security-group.id]
+  subnet_group_name  = aws_elasticache_subnet_group.redis-subnet-group.name
+  security_group_ids = [aws_security_group.db-security-group.id]
 }
